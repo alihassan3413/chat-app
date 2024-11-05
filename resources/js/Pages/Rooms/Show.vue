9b1e4870f22e3dd1bb154f8a3260a6b8c01af13d
Ali Hassan <alihassan478@icloud.com>
2024-11-05 12:18:04 +0500

    Room Creation for Chat

--- resources/js/Pages/Rooms/Show.vue
+++ resources/js/Pages/Rooms/Show.vue
@@ -1,6 +1,13 @@
 <script setup>
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import { Head } from "@inertiajs/vue3";
+
+defineProps({
+    room: {
+        type: Object,
+        required: true,
+    },
+});
 </script>
 
 <template>
@@ -16,9 +23,7 @@ import { Head } from "@inertiajs/vue3";
         <div class="py-12">
             <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
                 <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900">
-                        <!--  -->
-                    </div>
+                    <div class="p-6 text-gray-900">{{ room }}</div>
                 </div>
             </div>
         </div>
